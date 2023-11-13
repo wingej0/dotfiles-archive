a677549235e3e2998e4a9ef52d93fe7d28a02a1d
Jeff Winget <jeff@3dgradebook.com>
2023-11-13 16:11:22 -0700

    Frustrated with Qtile on Wayland

--- qtile/scripts/autostart.sh
+++ qtile/scripts/autostart.sh
@@ -1,16 +1,5 @@
 #!/bin/bash
 
-dbus-update-activation-environment --systemd \
-	WAYLAND_DISPLAY XDG_CURRENT_DESKTOP=$XDG_CURRENT_DESKTOP
-
-# Authentication dialog
-pkill -f /usr/lib/polkit-kde-authentication-agent-1
-/usr/lib/polkit-kde-authentication-agent-1 &
-
-# Start xdg-desktop-portal-wlr  
-pkill -f /usr/lib/xdg-desktop-portal-wlr
-/usr/lib/xdg-desktop-portal-wlr &
-
 kanshi &
 nm-applet &
 swayidle -w timeout 600 'swaylock-f' &
